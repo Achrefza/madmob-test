9a239bcdd73e3fee335d35de46455cb348b0f321
Achrefza <achref.ouerchfeni@gmail.com>
2026-03-13 02:47:21 +0100

    Improve mobile spacing between hero and MadBot controls

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -26,7 +26,7 @@ export default function HeroSection() {
         </p>
       </div>
 
-      <div className="absolute bottom-16 left-1/2 z-20 -translate-x-1/2 sm:bottom-20">
+      <div className="absolute bottom-28 left-1/2 z-20 -translate-x-1/2 sm:bottom-20">
         <button
           type="button"
           onClick={handleExploreCollective}
